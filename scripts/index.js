f6d7155880e721f42b02f3713f716df87df85c92
chucksef <chucksef@gmail.com>
2019-10-24 10:15:08 -0600

    v 0.3.1 Operator Buttons / Equation Readout functionality finished.

--- scripts/index.js
+++ scripts/index.js
@@ -8,12 +8,28 @@ function initialSetup(){
     mainDisplay = document.querySelector("#mainDisplay");
     
     //get all input buttons
-    inputButtons = Array.from(document.querySelectorAll("button.input"));
+    var inputButtons = Array.from(document.querySelectorAll("button.input"));
     //attach an onClick event listener to each input button
     for(i=0;i<inputButtons.length;i++){
         inputButtons[i].addEventListener("click", function(e) {
             //get clicked button's inner text and append it to the main display
             mainDisplay.innerText += e.target.innerText;
+        })
+    }
+
+    //get all operator buttons
+    var operatorButtons = Array.from(document.querySelectorAll("button.operator"));
+    //attach an onClick event listener to each input button
+    for(i=0;i<operatorButtons.length;i++){
+        operatorButtons[i].addEventListener("click", function(e) {
+            //get clicked button's inner text and append it to the main display
+            var operation = ` ${e.target.innerText}`;
+            var number = mainDisplay.innerText;
+            if (number != ""){
+                eqDisplay.innerText += " "+number;
+                eqDisplay.innerText += operation;
+                mainDisplay.innerText = "";
+            }
         })
     }
 
